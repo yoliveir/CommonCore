fb1a8684e69211ffa122e3eb0553b741f2d7cbe5
YuroliveCasa <yuriomacedo60@gmail.com>
2024-10-28 19:18:29 +0100

    Version 3 Minitalk, functional

---
+++ MileStone_2/MiniTalk/Version3/client/main.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yurolive <yurolive@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 16:29:44 by yurolive          #+#    #+#             */
+/*   Updated: 2024/10/28 16:50:09 by yurolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "client.h"
+
+t_global	g_server;
+
+void	signal_handler(int signum, siginfo_t *info, void *context)
+{
+	(void)signum, (void)context, (void)info;
+	if (signum == SIGUSR2)
+		g_server.ready_to_proceed = 1;
+	else if (signum == SIGUSR1)
+		ft_print_error("\rMesage was received by the server.");
+}
+
+int	main(int argc, char **argv)
+{
+	t_client			data;
+	int					msg_len;
+	struct sigaction	sa;
+	sigset_t			sigset;
+
+	parser(argc, argv);
+	init_data(argv, &data);
+	if (ping(data.server_pid) == 0)
+		return (0);
+	msg_len = ft_strlen(argv[2]);
+	ft_printf("MSG_LEN: [%d]\n", msg_len);
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGUSR1);
+	sigaddset(&sigset, SIGUSR2);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+	sa.sa_mask = sigset;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	send_signals(&msg_len, 32, &data);
+	send_message(data.msg, &data);
+	return (0);
+}
---
+++ MileStone_2/MiniTalk/Version3/server/server.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yurolive <yurolive@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 16:39:14 by yurolive          #+#    #+#             */
+/*   Updated: 2024/10/28 16:39:14 by yurolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "server.h"
+
+static int	get_bit_value(int signum)
+{
+	if (signum == SIGUSR1)
+		return (0);
+	return (1);
+}
+
+void	memory_reserve_to_store_signals(void)
+{
+	ft_printf("SIZE_MSG: [%d]\n", g_client.msg.size_message);
+	g_client.msg.message = malloc((g_client.msg.size_message + 1) * 1);
+	if (g_client.msg.message == NULL)
+		ft_print_error("Malloc_failed");
+	g_client.getting_header = 0;
+	g_client.getting_msg = 1;
+	g_client.sig_count = 0;
+}
+
+void	handle_header(int signum)
+{
+	const int	bit_value = get_bit_value(signum);
+
+	if (g_client.sig_count < HEADER_SIZE)
+	{
+		g_client.msg.size_message |= \
+			(bit_value << (HEADER_SIZE - 1 - g_client.sig_count));
+		g_client.sig_count++;
+	}
+	if (g_client.sig_count == HEADER_SIZE)
+		memory_reserve_to_store_signals();
+}
+
+static void	print_msg(void)
+{
+	write(1, HEADER_MESSAGE, 18);
+	write(1, g_client.msg.message, g_client.msg.size_message);
+	write(1, "\n", 1);
+}
+
+void	handle_msg(int signum)
+{
+	const int	bit_value = get_bit_value(signum);
+
+	if (g_client.sig_count % 8 < 8)
+	{
+		g_client.char_value |= (bit_value << (7 - (g_client.sig_count % 8)));
+		(g_client.sig_count)++;
+	}
+	if (g_client.sig_count % 8 == 0)
+	{
+		g_client.msg.message[g_client.msg_pos] = g_client.char_value;
+		g_client.char_value = 0;
+		g_client.msg_pos++;
+	}
+	if (g_client.sig_count / 8 == g_client.msg.size_message)
+	{
+		print_msg();
+		kill(g_client.client_pid, SIGUSR1);
+		free(g_client.msg.message);
+		g_client.msg.message = NULL;
+		ft_bzero(&g_client, sizeof(g_client));
+		g_client.getting_header = 1;
+	}
+}
